625f2bc0e8428850bd74e534bd48acbcb2ddd053
alohaisai <mm70aloha@gmail.com>
2019-08-06 12:01:59 +0900

    タブメニューを追加

--- js/sample.js
+++ js/sample.js
@@ -55,4 +55,19 @@ $('.js-modal-close').on('click', function() {
   $('#modal-bg').fadeOut(1000);
 });
 
+// タブメニュー
+$('.tab-nav a').on('click', function() {
+  if ($(this).hasClass('active')) {
+    return false;
+  }
+
+  $('.tab-nav a').removeClass('active');
+  $(this).addClass('active');
+
+  $('.tab-content > div').removeClass('active');
+  $('.tab-content > div').filter(this.hash).addClass('active');
+
+  return false;
+});
+
 });
